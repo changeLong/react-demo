4ecc961554db746082b5611fd637acca15ed1808
chenzhaolong <zlong5568863@qq.com>
2019-08-06 10:38:39 +0800

    test1

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-
+console.log(1);
 function calculateWinner(squares) {
     const lines = [
         [0, 1, 2],
