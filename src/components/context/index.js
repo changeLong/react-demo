0db52b94ed8f8a54367154a5e6ecf2d57a77c4a2
chenzhaolong <zlong5568863@qq.com>
2020-05-27 20:18:56 +0800

    context

--- src/components/context/index.js
+++ src/components/context/index.js
@@ -2,21 +2,21 @@
  * state demo
  */
 
-import React,{createContext} from 'react';
+import React, { createContext } from 'react';
 
 let init = {
-    count : 1
+    count: 1
 }
 
-let context = createContext({count : 2})
+let context = createContext({ count: 2 })
 
 
 class ContextChild extends React.Component {
     render(h) {
-        // console.log(this);
+        console.log(this);
         return (
             <>
-                <p>count : {this.context.count}</p>
+                <p >count : {this.context.count}</p>
             </>
         )
     }
@@ -24,15 +24,38 @@ class ContextChild extends React.Component {
 // 使用context.contextType = createContext()对象，可使组件能用this.context 访问 Provider 传进来的值
 ContextChild.contextType = context
 
-function contextComp() {
-    setInterval(() => {
-        init.count++
-    },1000)
-    return (
-        <context.Provider value={init}>
-            <ContextChild></ContextChild>
-        </context.Provider>
-    )
+// function contextComp() {
+//     setInterval(() => {
+//         init.count++;
+//         console.log(init.count);
+//     },1000)
+//     return (
+//         <context.Provider value={init}>
+//             <ContextChild></ContextChild>
+//         </context.Provider>
+//     )
+// }
+
+class contextComp extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            count : 1
+        }
+        this.toggle = () => {
+            this.setState({
+                init : 2
+            })
+        }
+    }
+    render() {
+        return (
+            <context.Provider value={this.state} >
+                <ContextChild ></ContextChild>
+                <p onClick={()=>{console.log(1111)}}>1111</p>
+            </context.Provider>
+        )
+    }
 }
 
 export default contextComp;
